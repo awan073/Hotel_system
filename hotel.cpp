07fa44cf2a1e6e9ef5e6e20ab59582399466a38f
awan073 <l1f20bsse0029@ucp.edu.pk>
2024-01-29 12:32:25 +0500

    ViewRoom has been added

--- hotel.cpp
+++ hotel.cpp
@@ -1,2 +1,8 @@
 function to search for room
 Where the customers can search for hotels based on location, date, and other preferences. Moreover, Customers can also view hotel details, including room types, prices, and amenities.  
+
+searchRoom Function
+Customers can also view hotel details, including room types, prices, and amenities
+
+
+
