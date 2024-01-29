df408610270ee292a936da53539abe282f40dd14
awan073 <l1f20bsse0029@ucp.edu.pk>
2024-01-29 12:15:47 +0500

    searchfunction

---
+++ hotel.cpp
@@ -0,0 +1,2 @@
+function to search for room
+Where the customers can search for hotels based on location, date, and other preferences. Moreover, Customers can also view hotel details, including room types, prices, and amenities.  
